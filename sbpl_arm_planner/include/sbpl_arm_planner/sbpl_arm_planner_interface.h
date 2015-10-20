e04c3f1848a28da12f734e7a6378f38ea2387ba4
Andrew Dornbush <andrew.dornbush@gmail.com>
2015-10-20 14:38:36 -0400

    reducing dependencies imposed by arm planner interface header

--- sbpl_arm_planner/include/sbpl_arm_planner/sbpl_arm_planner_interface.h
+++ sbpl_arm_planner/include/sbpl_arm_planner/sbpl_arm_planner_interface.h
@@ -1,57 +1,59 @@
-/*
- * Copyright (c) 2009, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2009, Willow Garage, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Willow Garage, Inc. nor the names of its
+//       contributors may be used to endorse or promote products derived from
+//       this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////
 
-/** \author Benjamin Cohen */
+/// \author Benjamin Cohen
 
 #ifndef _SBPL_ARM_PLANNER_INTERFACE_H_
 #define _SBPL_ARM_PLANNER_INTERFACE_H_
 
-#include <iostream>
+// standard includes
 #include <map>
-#include <time.h>
-#include <ros/ros.h>
-#include <tf/transform_datatypes.h>
-#include <kdl/frames.hpp>
-#include <angles/angles.h>
-#include <leatherman/utils.h>
-#include <leatherman/print.h>
-#include <leatherman/viz.h>
-#include <sbpl/planners/araplanner.h>
-#include <sbpl_arm_planner/environment_robarm3d.h>
-#include <sbpl_manipulation_components/post_processing.h>
+#include <string>
+
+// system includes
 #include <moveit/distance_field/propagation_distance_field.h>
-#include <geometry_msgs/Pose.h>
 #include <moveit_msgs/GetMotionPlan.h>
 #include <moveit_msgs/PlanningScene.h>
-#include <trajectory_msgs/JointTrajectory.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <ros/ros.h>
+#include <sbpl/headers.h>
+
+// project includes
+#include <sbpl_arm_planner/action_set.h>
+#include <sbpl_manipulation_components/collision_checker.h>
+#include <sbpl_manipulation_components/robot_model.h>
 
 namespace sbpl_arm_planner {
+
+class EnvironmentROBARM3D;
+class OccupancyGrid;
+class PlanningParams;
 
 class SBPLArmPlannerInterface
 {
