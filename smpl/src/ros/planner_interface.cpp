2433a412200304b2baf77639705f785c61e95955
Andrew Dornbush <andrew.dornbush@gmail.com>
2017-03-29 00:12:16 -0400

    add more complete search stats logging

--- smpl/src/ros/planner_interface.cpp
+++ smpl/src/ros/planner_interface.cpp
@@ -520,10 +520,13 @@ bool PlannerInterface::plan(double allowed_time, std::vector<RobotState>& path)
     // if a path is returned, then pack it into msg form
     if (b_ret && (solution_state_ids.size() > 0)) {
         ROS_INFO_NAMED(PI_LOGGER, "Planning succeeded");
-        ROS_INFO_NAMED(PI_LOGGER, "  Num Expansions: %d", m_planner->get_n_expands());
+        ROS_INFO_NAMED(PI_LOGGER, "  Num Expansions (Initial): %d", m_planner->get_n_expands());
+        ROS_INFO_NAMED(PI_LOGGER, "  Num Expansions (Final): %d", m_planner->get_n_expands_init_solution());
+        ROS_INFO_NAMED(PI_LOGGER, "  Epsilon (Initial): %0.3f", m_planner->get_initial_eps());
+        ROS_INFO_NAMED(PI_LOGGER, "  Epsilon (Final): %0.3f", m_planner->get_solution_eps());
+        ROS_INFO_NAMED(PI_LOGGER, "  Time (Initial): %0.3f", m_planner->get_initial_eps_planning_time());
+        ROS_INFO_NAMED(PI_LOGGER, "  Time (Final): %0.3f", m_planner->get_final_eps_planning_time());
         ROS_INFO_NAMED(PI_LOGGER, "  Path Length (states): %zu", solution_state_ids.size());
-        ROS_INFO_NAMED(PI_LOGGER, "  Initial Epsilon: %0.3f", m_planner->get_initial_eps());
-        ROS_INFO_NAMED(PI_LOGGER, "  Final Epsilon: %0.3f", m_planner->get_final_epsilon());
         ROS_INFO_NAMED(PI_LOGGER, "  Solution Cost: %d", m_sol_cost);
 
         path.clear();
