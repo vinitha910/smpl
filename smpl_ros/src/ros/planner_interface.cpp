8fb30049ca5bf561587074eba7d5433f176952f8
Andrew Dornbush <andrew.dornbush@gmail.com>
2017-11-27 22:14:42 -0500

    fix path visualization bugs

--- smpl_ros/src/ros/planner_interface.cpp
+++ smpl_ros/src/ros/planner_interface.cpp
@@ -1356,14 +1356,14 @@ auto PlannerInterface::makePathVisualization(
     for (size_t i = 0; i < path.size(); ++i) {
         auto markers = m_checker->getCollisionModelVisualization(path[i]);
 
-        for (auto& marker : ma) {
+        for (auto& marker : markers) {
             const float r = 0.1f;
             const float g = cinc * (float)(path.size() - (i + 1));
             const float b = cinc * (float)i;
             marker.color = visual::Color{ r, g, b, 1.0f };
         }
 
-        for (auto&& m : std::move(markers)) {
+        for (auto& m : markers) {
             ma.push_back(std::move(m));
         }
     }
